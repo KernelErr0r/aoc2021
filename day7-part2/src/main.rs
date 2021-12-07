51bdb40e8ea5352c0f54225d322151d8fd952424
KernelErr0r <villainkernelerror@gmail.com>
2021-12-07 08:50:13 +0100

    Add solution for part 2 of day 7

---
+++ day7-part2/src/main.rs
@@ -0,0 +1,36 @@
+use std::{fs::File, io::{BufReader, BufRead}, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let positions = reader.lines()
+        .filter_map(|line| line.ok())
+        .next()
+        .unwrap()
+        .split(',')
+        .map(str::parse)
+        .collect::<Result<Vec<usize>, _>>()?;
+
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    let min_cost: usize = (min ..= max).into_iter()
+        .map(|current_position| {
+            positions.iter()
+                .map(|position| {
+                    if current_position > *position {
+                        ((current_position - *position) * (current_position - *position + 1)) / 2
+                    } else {
+                        ((*position - current_position) * (*position - current_position + 1)) / 2
+                    }
+                })
+                .sum()
+        })
+        .min()
+        .unwrap();
+
+    println!("{}", min_cost);
+
+    Ok(())
+}
